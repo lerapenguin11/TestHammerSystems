0453baabba64c80d1823cd2bc416bcbc5be9852d
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-11-04 22:04:35 +0500

    fix url

--- app/src/main/java/com/example/testhammersystems/utilits/Const.kt
+++ app/src/main/java/com/example/testhammersystems/utilits/Const.kt
@@ -4,4 +4,4 @@ import com.example.testhammersystems.MainActivity
 
 lateinit var APP_ACTIVITY : MainActivity
 const val URL = "https://svergut.tw1.su/food-delivery/api/"
-const val URL_FILE = "http://192.168.43.78:3333/files/"
+const val URL_FILE = "${URL}files/"
